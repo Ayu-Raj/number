6e6950a9168ccf846d12ab6c171d9d33fb56135a
codewithayush <43947642+codewithayush@users.noreply.github.com>
2018-10-09 16:14:57 +0530

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("This progrma does subtraction\n");
+    printf("Enter 2 numbers\n");
+    scanf("%d %d", &a, &b );
+    printf("Sub=%d", a-b);
+    return 0;
+}

f3d7496abfc2df7f4a516cc1aebc37d1367d5154
Ayush Raj <ayushrajmathur@gmail.com>
2018-10-09 15:35:54 +0530

    Added add.c

---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("This progrma does summation\n");
+    printf("Enter 2 numbers\n");
+    scanf("%d %d", &a, &b );
+    printf("Sum=%d", a+b);
+    return 0;
+}
